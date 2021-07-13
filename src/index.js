d4f411b266f30b5ccc8c3d31bd0656af98c19fb6
Jocyline Magero <jmagero2017@gmail.com>
2021-07-13 15:59:16 -0500

    add function to iterate over the tasks array and populate an HTML list item element for each task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+const tasksDiv = document.getElementById('tasks');
 let tasks = [];
 class Task{
   constructor(index,description,completed){
@@ -13,3 +14,13 @@ let task2 = new Task(2,"second task",false)
 let task3 = new Task(3,"third task",true)
 let task4 = new Task(4,"forth task",true)
 tasks.push(task1,task2,task3,task4)
+const addTasks = (tasks) => {
+    for(let i = 0; i < tasks.length; i++){
+      let tableRow = document.createElement('tr');
+      let task = tasks[i]
+      tableRow.innerHTML= `<td>${task.index}</td>
+                            <td>${task.description}</td>`
+      tasksDiv.appendChild(tableRow);
+    }
+}
+addTasks(tasks);
