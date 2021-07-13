0a00df501a2c0571f0a80841c1f4e9d6767b0816
Jocyline Magero <jmagero2017@gmail.com>
2021-07-13 15:58:05 -0500

    set an array of some simple to do tasks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+let tasks = [];
 class Task{
   constructor(index,description,completed){
     this.index = index
@@ -7,3 +8,8 @@ class Task{
 
   }
 }
+let task1 = new Task(1,"first task",false)
+let task2 = new Task(2,"second task",false)
+let task3 = new Task(3,"third task",true)
+let task4 = new Task(4,"forth task",true)
+tasks.push(task1,task2,task3,task4)
