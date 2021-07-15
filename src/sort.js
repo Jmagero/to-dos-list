78775b283b9a040247f6d21a96f57e62ffcfe123
Jocyline Magero <jmagero2017@gmail.com>
2021-07-15 15:59:10 -0500

    Add function to sort tasks

---
+++ src/sort.js
@@ -0,0 +1,40 @@
+import { tasks } from './taskClass.js';
+import addTasks from './showTasks.js';
+import { saveTasks } from './localstorage.js';
+
+export default () => {
+  const draggables = document.querySelectorAll('.draggable');
+  let dragged;
+  draggables.forEach((e) => {
+    e.addEventListener('dragstart', (event) => {
+      dragged = event.target;
+    });
+
+    e.addEventListener('dragover', (event) => {
+      event.preventDefault();
+    });
+
+    e.addEventListener('drop', (event) => {
+      event.preventDefault();
+
+      for (let i = 0; i < event.target.classList.length; i += 1) {
+        if (event.target.classList[i] === 'droppable') {
+          const draggedIndex = tasks[dragged.children[0].children[0].children[0].id].index;
+          const droppedIndex = tasks[e.children[0].children[0].children[0].id].index;
+          const tempDescription = tasks[draggedIndex].description;
+          const tempDescription2 = tasks[droppedIndex].description;
+          const tempChecked = tasks[draggedIndex].completed;
+          const tempChecked2 = tasks[droppedIndex].completed;
+          tasks[draggedIndex].index = droppedIndex;
+          tasks[droppedIndex].index = draggedIndex;
+          tasks[droppedIndex].description = tempDescription;
+          tasks[draggedIndex].description = tempDescription2;
+          tasks[droppedIndex].completed = tempChecked;
+          tasks[draggedIndex].completed = tempChecked2;
+          addTasks();
+          saveTasks();
+        }
+      }
+    });
+  });
+};
